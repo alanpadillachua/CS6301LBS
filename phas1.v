a97f9f6e3ca4483fe4618412b22a70e6fcaeab56
Hanlin He <hanling.he@gmail.com>
2017-11-20 17:24:13 -0600

    Implemented transformation from string to 'nat -> option nat' mapping.

--- phas1.v
+++ phas1.v
@@ -1,3 +1,11 @@
+
+Require Import List.
+Require Import Ascii.
+Require Import String.
+
+Local Open Scope char_scope.
+Local Open Scope string_scope.
+
 Definition f : nat -> option nat := 
 fun n =>
   if Nat.eqb n 0 then Some 1 else
@@ -43,5 +51,52 @@ Compute matrix 1 0.
 Compute matrix 0 1.
 Compute matrix 1 1.
 Compute matrix 3 0.
+
+Fixpoint string_to_list (s : string): list ascii := 
+  match s with
+    | EmptyString => nil
+    | String h t => h :: string_to_list t
+end.
+
+Fixpoint ascii_to_nat_list (l : list ascii) : list nat :=
+  match l with
+  | nil => nil
+  | h :: t => nat_of_ascii h :: ascii_to_nat_list t
+end.
+
+Fixpoint list_to_map (l : list nat) (m : nat) : nat -> option nat :=
+  let f := fun x => None in
+  match l with
+  | nil => f
+  | h :: t => update (list_to_map t (S m)) m h
+  end.
+
+Definition string_to_map (word : string) : nat -> option nat :=
+  list_to_map (ascii_to_nat_list (string_to_list word)) O.
+
+Definition hello_world := string_to_map """Hello world!""".
+
+Definition ascii_of_nat_option (a : option nat) : option ascii :=
+  match a with
+  | None => None
+  | Some a' => Some (ascii_of_nat a')
+  end.
+
+Compute ascii_of_nat_option (hello_world 1).
+Compute ascii_of_nat_option (hello_world 2).
+Compute ascii_of_nat_option (hello_world 3).
+Compute ascii_of_nat_option (hello_world 4).
+Compute ascii_of_nat_option (hello_world 5).
+Compute ascii_of_nat_option (hello_world 6).
+Compute ascii_of_nat_option (hello_world 7).
+Compute ascii_of_nat_option (hello_world 8).
+Compute ascii_of_nat_option (hello_world 9).
+Compute ascii_of_nat_option (hello_world 10).
+Compute ascii_of_nat_option (hello_world 11).
+Compute ascii_of_nat_option (hello_world 12).
+Compute ascii_of_nat_option (hello_world 13).
+Compute ascii_of_nat_option (hello_world 14).
+
+
 
 
