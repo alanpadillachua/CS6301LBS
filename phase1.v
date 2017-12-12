7f516b4461056bdc125966c54700fc1a645c6d3c
Hanlin He <hanling.he@gmail.com>
2017-12-12 17:19:47 -0600

    Cleanup code.

--- phase1.v
+++ phase1.v
@@ -9,8 +9,9 @@
 Require Import List Nat Arith.
 Require Import Ascii String.
 Require Import Datatypes.
+Require Import FunctionalExtensionality.
 
-Local Open Scope char_scope.
+(*Local Open Scope char_scope.*)
 Local Open Scope string_scope.
 Open Scope bool_scope.
 
@@ -19,13 +20,7 @@ Definition char := option nat.
 Definition str := nat -> char.
 Definition str_matrix := nat -> str.
 
-(*
-Fixpoint Nat_eqb (x:nat) (y:nat) : bool :=
-match x with
-|0 => match y with 0 => true |_ => false end
-|S l => match y with S n => (Nat_eqb l n) | 0 => false end
-end.
-*)
+Definition eqdec (A:Type) := forall x y:A, {x=y}+{x<>y}.
 
 (** A generic "update" function for mapping "nat -> A". *)
 (* Note: The definition might be generalized again to update mapping "A -> B",
@@ -156,7 +151,7 @@ which is hard to compute directly from the mapping,
 so the previously computed value used here (not good). *)
 
 (* Right-shift a mapping by one and return a new mapping. *)
-Definition right_shift (m : nat -> option nat) (l : nat) : nat -> option nat :=
+Definition right_shift (m : str) (l : nat) : str :=
   fun n =>
     match n with
     | O => m l
@@ -189,6 +184,8 @@ Definition last (m : str) (length : nat) : char :=
   end.
 
 Eval compute in last hello_world hello_world_length.
+
+(* functional extensionality *)
 
 (** Generate right-shift permutation matrix of the string mapping. **)
 (*  Helper recursion definition, which keeps a constant length through recursion. *)
@@ -249,7 +246,7 @@ Eval compute in map_to_string hello_world_length last_col.
 
 (** Define whether two [str]s are reverse of each other at lenght "n". *)
 Definition reverse_str (n : nat) (f1 f2 : str) : Prop :=
-  forall (n1 n2: nat), n1 + n2 = n -> f1 n1 = f2 n2.
+  forall (n1 n2: nat), n1 + n2 = n <-> f1 n1 = f2 n2.
 
 (** Define whether two [str]s are the same. *)
 Definition same_str (f1 f2 : str) : Prop :=
@@ -258,7 +255,7 @@ Definition same_str (f1 f2 : str) : Prop :=
 (** Prove that the last column of the right-shift permutation matrix is the reverse of the original string mapping. **)
 Theorem last_col_reverse:
   forall (s : string) (l : nat) (m : nat -> option nat),
-    l = String.length s -> m = string_to_map s -> reverse_mapping l m (lasts l (map_to_conjugacy l m)).
+    l = String.length s -> m = string_to_map s -> reverse_str l m (lasts (map_to_conjugacy m l) l).
 Proof.
   intros.
   unfold map_to_conjugacy.
@@ -275,89 +272,83 @@ Definition bwt' (s : string) (sort: (nat -> nat -> option nat) -> (nat ->  nat -
 (* An example bwt without sorting. *)
 Eval compute in bwt' hello_world_str (fun x => x).
 
-(**
-"Hello World!"
-"!Hello World"
-"d!Hello Worl"
-"ld!Hello Wor"
-"rld!Hello Wo"
-"orld!Hello W"
-"World!Hello "
-" World!Hello"
-"o World!Hell"
-"lo World!Hel"
-"llo World!He"
-"ello World!H"
-*)
 
-Definition zip {A : Set} (map : nat -> A) : nat -> prod A nat :=
-  fun n => pair (map n) n.
-
-Definition eqdec (A:Type) := forall x y:A, {x=y}+{x<>y}.
-
-Definition cmp (A:Type) := A -> A -> Prop.
-
-Fixpoint indexOf {A : Type} (eq : eqdec A) (map : nat -> A) (len : nat) (target : A) : option nat :=
-  let fix indexOf' {A : Type} (eq : eqdec A) (map : nat -> A) (target : A) (i : nat) : option nat :=
-    match i with
-    | O => None
-    | S i' => if eq (map i) target then Some i else indexOf' eq map target (i')
-    end in
-  indexOf' eq map target len.
-
-(*
-Definition standard_permutation :=
-  let fix standard_permutation' (start_index : nat)
-                                (f : option nat -> option nat)
-                                (A : Type)
-                                (eq : cmp (@prod A nat))
-                                (zipped_bwt sorted_bwt : nat -> @prod A nat) :
-                                option nat -> option nat :=
-    match sorted_bwt with
-    | nil => f
-    | h :: t => standard_permutation' (S start_index) 
-                                      (update OptionNatOrder.eqb f (Some start_index) (indexOf eq zipped_bwt h))
-                                      A eq zipped_bwt t
-    end in
-  standard_permutation' O (fun (_: option nat) => None).
-*)
-
-(*  Currently we used [nat] represent a letter to simplify the Sorting part. 
-    But if we define a [TotalOrder] on [ascii], the conversion would be unnecessary.
-    Or we can assume the sort function sort on ascii directly. *)
-
-(* None has lowest rank in the implementation of cmp. *)
-
-(* Sort function set up. *)
-
-Definition sorter {A:Type} (leq: cmp A) (sort: (nat -> A) -> (nat -> A)) (len: nat) : Prop :=
-  forall f n, S n < len -> leq (sort f n) (sort f (S n)).
-
-Fixpoint count {A:Type} (eq:eqdec A) (x:A) (f: nat -> A) (len: nat) :=
-  match len with
-  | O => O
-  | S m => if eq (f m) x then S (count eq x f m) else count eq x f m
+(** Option nat order. *)
+Definition optnat_leq (x y : option nat) :=
+  match y with None => True | Some n =>
+    match x with None => False | Some m => m <= n end
   end.
 
-Definition permuter {A:Type} (eq: eqdec A) (sort: (nat -> A) -> (nat -> A)) (len: nat) :=
-  forall f x, count eq x (sort f) len = count eq x f len.
+(** Option nat equility. *)
+Definition optnat_eqb (a1 a2 : option nat) :=
+  match a1, a2 with
+  | None, None => True
+  | None, _ => False
+  | _, None => False
+  | Some a1', Some a2' => Nat.eqb a1' a2' = true
+  end.
 
+(** Generic sequence leq ordering. *)
+Fixpoint leq_seq {A:Type} (eq leq : A -> A -> Prop) (k : nat) (s1 s2 : nat -> A) : Prop :=
+  match k with
+  | O => leq (s1 O) (s2 O)
+  | S m => (eq (s1 O) (s2 O) /\ leq_seq eq leq m (fun i => s1 (S i)) (fun i => s2 (S i)))
+            \/ leq (s1 O) (s2 O)
+  end.
 
+(** Generic sequence equility based on leq. *)
+Fixpoint eq_seq {A:Type} (eq : A -> A -> Prop) (k : nat) (s1 s2 : nat -> A) : Prop :=
+  match k with
+  | O => eq (s1 O) (s2 O)
+  | S m => eq (s1 O) (s2 O) /\ eq_seq eq m (fun i => s1 (S i)) (fun i => s2 (S i))
+  end.
 
+(** Option nat sequence leq ordering based on leq_seq. *)
+Definition leq_optnatctx (k : nat) (s1 s2 : str) : Prop :=
+  leq_seq optnat_eqb optnat_leq k s1 s2.
 
+(** Option nat sequence equility  based on eq_seq. *)
+Definition eq_optnatctx (k : nat) (s1 s2 : str) : Prop :=
+  eq_seq optnat_eqb k s1 s2.
 
-(* Possible final theorem. *)
-Definition bwt {A:Type} (s : string) (sort: (nat -> A) -> (nat -> A)) (len: nat): string := s.
+(** String matrix sequence equility  based on eq_seq. *)
+Definition eq_matrix (k : nat) (s1 s2 : str_matrix) : Prop :=
+  eq_seq (eq_optnatctx k) k s1 s2.
 
-Definition inv_bwt {A:Type} (s : string) (sort: (nat -> A) -> (nat -> A)) (len: nat): string := s.
+(** String leq ordering for all context. *)
+Definition leq_optnatseq s1 s2 := forall k, leq_optnatctx k s1 s2.
 
-Theorem lossless :
-  forall (s : string) {A:Type} (eq: eqdec A) (leq : cmp A) (sort: (nat -> A) -> (nat -> A)) (len: nat),
-    sorter leq sort len -> permuter eq sort len -> inv_bwt (bwt s sort len) sort len = s.
+(** The image of function f is sorted with respect to order relation R. *)
+Definition sorted {A:Type} (R: A -> A -> Prop) (f: nat -> A) :=
+  forall i, R (f i) (f (S i)).
+
+(* If relation R is reflexive and transitive, then image(f)'s ordering is also transitive. *)
+Definition reflexive {A:Type} (R: A -> A -> Prop) :=
+  forall a, R a a.
+
+Definition transitive {A:Type} (R: A -> A -> Prop) :=
+  forall a c b, R a b -> R b c -> R a c.
+
+Theorem sorted_transitive {A:Type}
+  (R: A -> A -> Prop) (RE: reflexive R) (TR: transitive R) :
+  forall (f: nat -> A) (SRT: sorted R f) i j, i <= j -> R (f i) (f j).
 Proof.
-  intros s.
-  reflexivity.
+  intros f SRT i j. revert i. induction j; intros; inversion H.
+    apply RE.
+    apply RE.
+    eapply TR. apply IHj. assumption. apply SRT.
 Qed.
+
+(* Permutation functions are injective and surjective. *)
+Definition injective {A B:Type} (f: A -> B) :=
+  forall i j, f i = f j -> i=j.
+
+Definition surjective {A B:Type} (f: A -> B) :=
+  forall j, exists i, f i = j.
+
+Definition bijective {A B:Type} (f: A -> B) :=
+  injective f /\ surjective f.
+
 
 
 (* Return the character at position i of string m *)
@@ -365,71 +356,42 @@ Definition lambda (m:nat -> option nat) (i:nat) := m i.
 
 (* Returns the position of an alphabet 'char' in string 'm' of length 'length' *)
 Fixpoint get_pos (eq:eqdec (option nat)) (m:nat -> option nat) (length:nat) (char: option nat) : nat :=
-match length with
-|0 => if (eq (m 0) char) then 0 else (S length)
-|S l => if (eq (m length) char) then length else (get_pos eq m l char)
-end. 
-
-(* Returns a 1-to-1 mapping \u03c0 as described in the White paper *) 
-Definition pi (eq:eqdec (option nat)) (length:nat) (sort: (nat -> (option nat)) -> (nat -> (option nat))) (m: nat -> option nat) : (nat -> nat):=
-fun (x:nat) => get_pos eq (sort m) length (m x).
+  match length with
+  |0 => if (eq (m 0) char) then 0 else (S length)
+  |S l => if (eq (m length) char) then length else (get_pos eq m l char)
+  end. 
 
 (* Introduce sorted string as assumption, not sort it inside. *)
-Definition pi' (eq:eqdec (option nat)) (length:nat) (m sorted_m: str) : (nat -> nat):=
+Definition pi (eq:eqdec (option nat)) (length:nat) (m sorted_m: str) : (nat -> nat):=
 fun (x:nat) => get_pos eq (sorted_m) length (m x).
 
 Definition context (k:nat) (m: nat -> (option nat)) : (nat -> (option nat)) :=
 fun (x:nat) => if (x <? k) then (m x) else None.
 
-(** Transform a nat list to a index -> nat mapping, i.e. nat -> option nat. *)
-Fixpoint list_to_map' (l : list (option nat)) (start_index : nat) : nat -> option nat :=
-  let f := fun _ => None in
-  match l with
-  | nil => f
-  | h :: t => update (list_to_map t (S start_index)) start_index h
-  end.
-
-Fixpoint inverse_bwt' (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m:nat -> (option nat)) (sort: (nat -> (option nat)) -> (nat -> (option nat))): (nat-> nat) :=
-let p' := (pi eq k sort m) in
-let f := fun _ => (S tot_length) in
-match k with
-|O => update f 0 (p' i)
-|S l => let rec :=  (inverse_bwt' eq tot_length i l m sort) in 
-       update rec k (p'(rec l)) (*  (lambda m ((pi eq l sort rec) k))   *)
-end.
-
 (* Use [pi'] instead of [pi]. *)
-Fixpoint inverse_bwt'' (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m sorted_m :nat -> (option nat)) : (nat-> nat) :=
-let p' := (pi' eq k m sorted_m) in
+Fixpoint inverse_bwt' (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m sorted_m :nat -> (option nat)) : (nat-> nat) :=
+let p' := (pi eq k m sorted_m) in
 let f := fun _ => (S tot_length) in
 match k with
 |O => f
-|S l => let rec :=  (inverse_bwt'' eq tot_length i l m sorted_m) in 
+|S l => let rec :=  (inverse_bwt' eq tot_length i l m sorted_m) in 
        update rec k (p'(rec l)) (*  (lambda m ((pi eq l sort rec) k))   *)
 end.
 
+(** Use [inverse_bwt']. *)
+Definition inverse_bwt (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m sorted_m: str) :=
+fun n => lambda m (inverse_bwt' eq tot_length i k m sorted_m n).
 
-Fixpoint inverse_bwt (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m: str) (sort: (nat -> (option nat)) -> (nat -> (option nat))): (nat-> option nat) :=
-fun n => lambda m (inverse_bwt' eq tot_length i k m sort n).
-
-
-Definition inverse_bwt_s (eq:eqdec (option nat)) (tot_length:nat) (i k:nat) (m sorted_m: str) :=
-fun n => lambda m (inverse_bwt'' eq tot_length i k m sorted_m n).
-
-Definition sorted_m (m m_sorted: str) (k : nat) : Prop := True.
+(* We tried to define what is a sorted string and string matrix. *)
+Definition sorted_str (m m_sorted: str) (k : nat) : Prop := True.
 Definition sorted_matrix (matrix : str_matrix) (k : nat) : Prop := True.
-
-Lemma matrix (m : nat -> nat -> option nat) (length : nat) (eq:eqdec (option nat))  (sort: (nat -> (option nat)) -> (nat -> (option nat))) :
-  forall (m : nat -> nat -> option nat) (k i: nat) ,
-    i <= length ->
-    sorted_matrix m k ->
-    same_str (context k (m i)) (inverse_bwt eq length i k (m i) sort).
-Proof.
-  intros.
-Admitted.
 
 Definition length_mapping (s : str) (length : nat): Prop :=
   forall l, l > length -> s l = None.
+
+(* Length is then a property like the following: *)
+Definition haslen {A:Type} (f : nat -> option A) (len: nat) :=
+  forall i, len <= i <-> f i = None.
 
 Definition prepend (s : str) (c : option nat) : str :=
   fun n =>
@@ -442,15 +404,25 @@ Definition concat (s1 s2: str) (l1 l2 : nat) : str :=
   fun n => if leb n l1 then s1 n else s2 (n - l1).
 
 Lemma context_k (w: str_matrix) (L : str) (length : nat) :
-  forall (k i : nat) ,
+  forall (k i : nat),
     k <= length ->
     i <= length ->
     same_str (context (S k) (right_shift (w i) length)) (prepend (context k (w i)) (lambda L i)).
 Proof.
+  induction k.
+  - intros. unfold right_shift.
 Admitted.
 
 Definition right_shift_matrix (w: str_matrix) (length : nat) : str_matrix :=
   fun n => right_shift (w n) length.
+
+Lemma Right_Shift (w : str_matrix) (length : nat) (sort : nat -> str_matrix -> str_matrix) :
+  forall t , 
+    t < length ->
+    sorted (leq_optnatctx t) w ->
+    eq_matrix (S t) (sort 1 (right_shift_matrix w length)) (sort (S t) (right_shift_matrix w length)).
+Proof.
+Admitted.
 
 Lemma sort_matrix (w: str_matrix) (length : nat) (k : nat) (sort : nat -> str_matrix -> str_matrix) (pi : nat -> nat) :
   sorted_matrix w k ->
@@ -458,44 +430,23 @@ Lemma sort_matrix (w: str_matrix) (length : nat) (k : nat) (sort : nat -> str_ma
 Proof.
 Admitted.
 
-Lemma matrix' (w: str_matrix) (L L_sorted: str) (length : nat) (eq:eqdec (option nat))
-              (sort : str_matrix -> str_matrix) :
-  forall (k i: nat) ,
-    i <= length ->
-    length_mapping L length ->
-    length_mapping L_sorted length ->
-    sorted_matrix w k ->
-    same_str L (lasts w length) ->
-    sorted_m L L_sorted length ->
-    same_str (context k (w i)) (inverse_bwt_s eq length i k L L_sorted).
-Proof.
-  induction k.
-  intros. 
-  unfold same_str.
-  
-  unfold context.
-  unfold inverse_bwt_s.
-  intro n. destruct n; simpl.
-  unfold lambda. rewrite H0. reflexivity. auto.
-  unfold lambda. rewrite H0. reflexivity. auto.
-  
-  intros.
-  apply IHk in H.
-  
-  
-Admitted.
-
-
-Lemma matrix'' (w: str_matrix) (L L_sorted: str) (length : nat) (eq:eqdec (option nat))
-              (sort : str_matrix -> str_matrix) (k : nat) :
+Lemma matrix' (w1 w2 : str_matrix) (L L_sorted: str) (length : nat) (eq:eqdec (option nat)) (k : nat) :
   forall (i: nat) ,
     i <= length ->
     length_mapping L length ->
     length_mapping L_sorted length ->
-    sorted_matrix w k ->
-    same_str L (lasts w length) ->
-    sorted_m L L_sorted length ->
-    forall x : nat, x <= k -> same_str (context x (w i)) (inverse_bwt_s eq length i x L L_sorted).
+    sorted (leq_optnatctx k) w1 ->
+    same_str L (lasts w1 length) ->
+    sorted_str L L_sorted length ->
+    forall x : nat, x <= k -> same_str (context x (w1 i)) (inverse_bwt eq length i x L L_sorted).
 Proof.
-
+  induction x.
+  intros. 
+  unfold same_str.
+  unfold context.
+  unfold inverse_bwt.
+  intro n. destruct n; simpl.
+  unfold lambda. rewrite H0. reflexivity. auto.
+  unfold lambda. rewrite H0. reflexivity. auto.
+Abort.
 
